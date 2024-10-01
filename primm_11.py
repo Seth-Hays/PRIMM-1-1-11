7a794aaeda595831e046bb4de25e17b4ec1a3501
I Run With Scissors <147119210+Seth-Hays@users.noreply.github.com>
2024-10-01 21:44:39 +0000

    Fixed the code

--- primm_11.py
+++ primm_11.py
@@ -6,12 +6,12 @@ Seth - September 2024
 
 def main():
     # Delcaring variables and asking for input
-    num1: int = input("Enter a number: ")
-    num2: int = input("Enter another number: ")
+    num1: int = int(input("Enter a number: "))
+    num2: int = int(input("Enter another number: "))
     total: int = num1 + num2
 
     # Printing what you input
-    print(f"{num1} ** {num2} = {total}")
+    print(f"{num1} + {num2} = {total}")
 
 if __name__ == "__main__":
   main()
